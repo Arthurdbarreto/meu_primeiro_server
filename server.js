324399a8cfa7430a527128ff7ff4c58a4c041c67
Arthur da Silva Barreto <arthurbarreto851@gmail.com>
2026-04-17 00:14:04 +0000

    requisição /arthur

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const http = require('http');
-
-console.log(http)
+const fs = require('fs')
+const name = "Arthur"
 
 server = http.createServer((req, res)=>{
-    res.end("Servidor no AR!!!")
+    console.log(req.url)
+    file = fs.readFileSync("./index.html", "utf-8")
+    file = file.replace("{{mudar}}", name)
+    res.writeHead(200, { 'Content-Type': 'text/html'});
+    res.end(file)
 })
 
 server.listen(3000, console.log("rodando na 3000"))
