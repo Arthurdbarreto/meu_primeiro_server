c5676af030d6d7c228015548f1bb11b42a5f18cc
Arthur da Silva Barreto <arthurbarreto851@gmail.com>
2026-04-17 00:15:17 +0000

    requisição /req.url

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const name = "Arthur"
 server = http.createServer((req, res)=>{
     console.log(req.url)
     file = fs.readFileSync("./index.html", "utf-8")
-    file = file.replace("{{mudar}}", name)
+    file = file.replace("{{mudar}}", req.url)
     res.writeHead(200, { 'Content-Type': 'text/html'});
     res.end(file)
 })
